f66d0ec2b9e97794c166743ff817f4cc2f381ff9
Lucas Brogni <lucasbrogni16@gmail.com>
2020-06-15 19:55:35 -0300

    test: add tests to login and logout

--- tests/User.test.js
+++ tests/User.test.js
@@ -3,7 +3,8 @@ const userCtrl = require('../src/controllers/users');
 const userRepository = require('../src/repository/users');
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
-
+const request = require('supertest');
+const app = require('../src/index')
 describe('User Tests', () => {
 
     beforeAll(async () => {
@@ -17,7 +18,7 @@ describe('User Tests', () => {
     });
 
     const userMock = { name: 'Lucas Brogni', email: 'lucasbrogni16@gmail.com', password: 'bananas_de_pijamas' };
-    let userCreated;
+    let userCreatedCode, userCreated;
     it('Factory should return a code ', async () => {
         const user = await userFactory(userMock);
         expect(user.code).toBeDefined();
@@ -38,8 +39,8 @@ describe('User Tests', () => {
         const user = await userFactory(userMock);
         const response = await userRepository.create(user);
         expect(response).toHaveProperty('_id');
-
-        userCreated = response.code;
+        userCreated = response;
+        userCreatedCode = response.code;
     })
 
     it('Should failed to create a prospect ', async () => {
@@ -49,9 +50,22 @@ describe('User Tests', () => {
 
 
     it('Should find an user by e-mail ', async () => {
-        const user = await  userRepository.findUserByEmail(userMock.email);
+        const user = await userRepository.findUserByEmail(userMock.email);
         expect(user).toHaveProperty('_id');
         expect(user.email).toBe(userMock.email);
+    })
+
+    it('Should make login', async () => {
+        let { status , body } = await request(app).post('/users/sign-in').send({ email: userMock.email, password: userMock.password });
+        expect(status).toBe(200);
+        expect(body).toHaveProperty('auth', true);
+        expect(body).toHaveProperty('user');
+    })
+
+    it('Should finish session ' , async () => { 
+        let { status } = await request(app).post('/users/logout')
+
+        expect(status).toBe(204);
     })
 
     it('Should list an array of users ', async () => {
@@ -60,15 +74,15 @@ describe('User Tests', () => {
     })
 
     it('Should list one result', async () => {
-        let response = await userRepository.findOne(userCreated);
+        let response = await userRepository.findOne(userCreatedCode);
 
         expect(response).toHaveProperty('_id');
-        expect(response.code).toBe(userCreated)
+        expect(response.code).toBe(userCreatedCode)
         expect(response.name).toBe('Lucas Brogni');
     })
 
     it('Should update an user ', async () => {
-        const mock = { ...userMock, code: userCreated, name: 'José' };
+        const mock = { ...userMock, code: userCreatedCode, name: 'José' };
         let result = await userRepository.update(mock);
 
         expect(result.ok).toBe(1);
@@ -83,7 +97,7 @@ describe('User Tests', () => {
     })
 
     it('Should remove an user ', async () => {
-        const response = await userRepository.remove(userCreated);
+        const response = await userRepository.remove(userCreatedCode);
         expect(response).toHaveProperty('deletedCount');
         expect(response.deletedCount).toEqual(1);
     })
