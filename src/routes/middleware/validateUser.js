b71615c587d8b4c4531fae399253e43d5f448731
Lucas Brogni <lucasbrogni16@gmail.com>
2020-06-15 19:07:22 -0300

    feat: add token validation

--- src/routes/middleware/validateUser.js
+++ src/routes/middleware/validateUser.js
@@ -1,3 +1,18 @@
-module.exports = (req, res, next) => { 
-    return next();
+
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    let token = req.headers['x-access-token'];
+
+    if (!token) {
+        return res.status(403).send('No token provided');
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(401).json({ auth: false, message: 'Failed to authenticate token.' });
+
+        req.$userCode = decoded.code;
+        next();
+    });
+
 }
