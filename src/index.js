aa29f5ddd9a79973806dd98d8de46056391cca24
Lucas Brogni <lucasbrogni16@gmail.com>
2020-06-21 19:51:38 -0300

    feat: add cross origin

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const routes = require('./routes');
-const PORT = process.env.PORT || 3000;
-const configDB = require('./config/db')
+const routes = require("./routes");
+const PORT = process.env.PORT || 8080;
+const configDB = require("./config/db");
 require("dotenv-safe").config({
-    allowEmptyValues: true,
+  allowEmptyValues: true,
 });
-const session = require('express-session');
-const connectSession = require('./config/redis')
+const session = require("express-session");
+const connectSession = require("./config/redis");
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session(connectSession(session)));
 
-app.use('/', routes);
+app.use("/", routes);
 app.listen(PORT, async () => {
-    await configDB();
+  await configDB();
 });
 
 module.exports = app;
-
